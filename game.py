a0b6a681e6da03cef606af9032f345c3ffdc9321
braden <leachbr787@northwested.org>
2025-03-19 13:22:34 -0400

    Update game.py

--- game.py
+++ game.py
@@ -3,13 +3,15 @@
 import pygame
 import sys
 import config # Import the config module 
+
+# -- initilize -- #
 def init_game (): 
     pygame.init()
     screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT)) # Use constants from config
     pygame.display.set_caption(config.TITLE)
     return screen
 
-
+# -- Events -- #
 def handle_events ():
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
@@ -19,13 +21,72 @@ def handle_events ():
              return False
     return True
 
+
+# -- Draw Circle -- #
+def draw_circle(screen, center, radius, color, thickness):
+    # --- Draw circle on window --- #
+    pygame.draw.circle(screen, color, center, radius, thickness)
+
+
+# -- Draw Rectangle -- #
+def draw_rectangle(screen, rect, color, thickness, border_radius):
+    # --- Draw rect on window --- #
+    pygame.draw.rect(screen, color, rect, thickness, border_radius)
+
+
+# -- Draw Polygon -- #
+def draw_poly(screen, color, points,thickness):
+    # --- Draw polygon on window --- #
+    pygame.draw.polygon(screen, color, points,thickness)
+
+
+
+# -- Main -- #
 def main():
    screen = init_game()
    clock = pygame.time.Clock() # Initialize the clock here
+
+
+
    running = True
    while running:
       running = handle_events()
+
+      # -- Handle Key Pressed For Movement -- #
+
       screen.fill(config.WHITE) # Use color from config
+
+      #-- Click Icon --#
+      if pygame.mouse.get_pressed()[0] == False:
+
+         # --- Circle Drawn out line --- #
+         circle_center = (370, 295)
+         circle_radius = 100
+         circle_color = config.BLACK
+         circle_thick = 4
+         draw_circle(screen, circle_center, circle_radius, circle_color, circle_thick)
+
+         # --- triangle Drawn --- #
+         points = [(340, 250), #top left
+                     (340, 340), #bottom left
+                     (430, 300)  #right
+                     ]
+         thickness = 0
+         draw_poly(screen, config.BLACK, points,thickness)
+
+         # --- triangle Drawn outline --- #
+         points = [(340, 250), #top left
+                     (340, 340), #bottom left
+                     (430, 300)  #right
+                     ]
+         thickness = 4
+         draw_poly(screen, config.WHITE, points,thickness)
+
+
+
+
+
+
       pygame.display.flip()
 
       # -- Limit the frame rate to the specified frames per second (FPS) -- #
